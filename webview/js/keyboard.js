4898976c39cce49893c302ea8098600b15124a6a
Phyks <webmaster@phyks.me>
2015-09-28 20:11:07 +0000

    Do not handle keypresses if Disty is not ready

--- webview/js/keyboard.js
+++ webview/js/keyboard.js
@@ -1,14 +1,19 @@
-// EDIT: Edit this according to your needs
+// This server and port is used to initiate the websocket to control Disty (and only for this purpose). "disty" should resolve to your Disty robot.
 var config = {
     "server": "disty",
     "port": 80
 };
 
 
+// Whether everything is ready or not
+var ready = false;
+
+
 // Websocket handler
 var keyboard_ws = new WebSocket("ws://"+config["server"]+":"+config["port"], ['binary', 'base64']);
 keyboard_ws.onopen = function(evt) {
     document.getElementById("move").innerHTML = "Waiting for commands…";
+    ready = true;
 };
 
 keyboard_ws.onmessage = function(evt) {
@@ -32,7 +37,11 @@ _prevKeyDown = {}  // Store keys currently pressed
 
 function handleMove(e) {
     var ev = e || window.event;
-    ev.preventDefault();
+
+    if (!ready) {
+        // If not ready, forget about handling keypresses
+        return;
+    }
 
     if (_prevKeyDown[ev.key] == true) {  // Avoid multiple presses
         return;
@@ -78,8 +87,11 @@ function handleMove(e) {
 function handleStop(e) {
     var ev = e || window.event;
 
-    keyboard_ws.send("STOP");
-    document.getElementById("move").innerHTML = "Waiting for commands…";
+    if (ready) {
+        // If ready, STOP Disty and show a "waiting for commands" text
+        keyboard_ws.send("STOP");
+        document.getElementById("move").innerHTML = "Waiting for commands…";
+    }
 
     _prevKeyDown[ev.key] = false;
 }
